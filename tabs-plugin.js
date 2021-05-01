ba2b39c258ed842280459ee49147331de62bcf03
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-05-01 23:20:21 +0300

    made basic functionality

--- tabs-plugin.js
+++ tabs-plugin.js
@@ -1,6 +1,35 @@
 "use strict";
 
 const refs = {
-  controls: document.querySelector("[data-controls]"),
-  panes: document.querySelector("[data-panes]"),
+  controls: document.querySelector("#tabs-1 [data-controls]"),
+  panes: document.querySelector("#tabs-1 [data-panes]"),
 };
+
+console.log(refs);
+
+refs.controls.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "A") {
+    return;
+  }
+
+  const currentActiveControl = refs.controls.querySelector(
+    ".controls__item--active"
+  );
+
+  if (currentActiveControl) {
+    currentActiveControl.classList.remove("controls__item--active");
+
+    const paneId = currentActiveControl.getAttribute("href").slice(1);
+    const pane = refs.panes.querySelector(`#${paneId}`);
+    pane.classList.remove("pane--active");
+  }
+
+  const controlItem = e.target;
+  controlItem.classList.add("controls__item--active");
+
+  const paneId = controlItem.getAttribute("href").slice(1);
+  const pane = refs.panes.querySelector(`#${paneId}`);
+  pane.classList.add("pane--active");
+});
